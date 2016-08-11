6f584b55d48bfb327682df9fa0ca23b1df96848b
LishenPan <panlishen@gmail.com>
2016-08-11 16:18:38 +0800

    Add files via upload

---
+++ ip_finder.cpp
@@ -0,0 +1,499 @@
+﻿#include "ip_finder.hpp"
+
+#include <fcntl.h>  
+#include <sys/types.h>  
+#include <math.h>  
+#include <stdio.h>  
+#include <string.h>  
+#include <sys/stat.h>  
+#include <errno.h>   
+#include <locale>
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+
+#else
+#include <unistd.h>  
+#include <sys/mman.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iconv.h>
+#endif
+
+#define OUTLEN 255
+#define SHARE_MEMORY_FILE "/tmp/qqwry.dat"
+#define UNKNOWN "Unknown"
+#define SHARE_MEMORY_SIZE 10485760 // 10M > qqwry.dat
+#define RECORD_LEN 7
+
+namespace is{
+namespace common{
+namespace location{
+ip_finder::ip_finder()
+  : pshare_(NULL)
+  , pbegin_(NULL)
+  , pend_(NULL)
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+  , hFileMap_(NULL)
+#endif
+  , total_record_(0)
+{
+
+}
+
+ip_finder::~ip_finder()
+{
+  destroy();
+}
+
+int ip_finder::initialize(const std::string& file)
+{
+  if (file.empty()) return -1;
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+	HANDLE hFile = CreateFile(file.c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_FLAG_RANDOM_ACCESS, NULL);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		return GetLastError();
+	}
+
+	hFileMap_ = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+	if (hFileMap_ == NULL) {
+		return GetLastError();
+	}
+
+	DWORD dwFileSizeHigh;
+	__int64 qwFileSize = GetFileSize(hFile, &dwFileSizeHigh);
+
+	CloseHandle(hFile);
+
+	pshare_ = (PBYTE)MapViewOfFile(hFileMap_, FILE_MAP_READ, 0, 0, 0);
+	if (pshare_ == NULL) {
+		return GetLastError();
+	}
+#else
+  std::string strutf8_province("\347\234\201");
+  std::string strutf8_city("\345\270\202");
+  std::string strutf8_xizang("\350\245\277\350\227\217");
+  std::string strutf8_xinjiang("\346\226\260\347\226\206");
+  std::string strutf8_neimeng("\345\206\205\350\222\231\345\217\244");
+  std::string strutf8_ningxia("\345\256\201\345\244\217");
+  std::string strutf8_guangxi("\345\271\277\350\245\277");
+
+  utf8togb2312(strutf8_province, strgb_province_);
+  utf8togb2312(strutf8_city, strgb_city_);
+  utf8togb2312(strutf8_xizang, strgb_xizang_);
+  utf8togb2312(strutf8_xinjiang, strgb_xinjiang_);
+  utf8togb2312(strutf8_neimeng, strgb_neimeng_);
+  utf8togb2312(strutf8_ningxia, strgb_ningxia_);
+  utf8togb2312(strutf8_guangxi, strgb_guangxi_);
+
+	if (pshare_ == NULL) {
+		int fd = open(file.c_str(), O_RDONLY); 
+		if (-1 == fd) {
+			return errno;
+		}
+
+		struct stat sb;
+		if (-1 == fstat(fd, &sb)) {
+			return errno;
+		}
+
+		void* pmmap = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+		if (MAP_FAILED == pmmap) {
+			return errno;
+		}
+
+		pshare_ = (unsigned char*)pmmap;
+ 
+		close(fd);   
+	}
+#endif
+
+	pbegin_ = pshare_ + get_long4(pshare_);
+  pend_ = pshare_ + get_long4(pshare_ + 4);
+  total_record_ = (get_long4(pshare_ + 4) - get_long4(pshare_)) / RECORD_LEN + 1;  
+
+	return 0;
+}
+
+void ip_finder::destroy()
+{ 
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+	if (pshare_ != NULL) {
+		UnmapViewOfFile(pshare_);
+		pshare_ = NULL;
+	}
+
+	if (hFileMap_ != INVALID_HANDLE_VALUE) {
+		CloseHandle(hFileMap_);
+		hFileMap_ = INVALID_HANDLE_VALUE;
+	}
+#else
+	if (pshare_ != NULL) {
+    munmap(pshare_, SHARE_MEMORY_SIZE);
+	  pshare_ = NULL;
+  }
+#endif
+}
+
+bool ip_finder::get_ipinfo(const std::string& ipstr, std::string& province, std::string& city)
+{
+  std::string country;
+  std::string area;
+  if (get_ip_original_info(ipstr, country, area)) {
+    size_t pos = std::string::npos;
+    do {
+    	pos = country.find(strgb_province_, 0);
+		if (pos >= 4 && pos != std::string::npos) {
+		  province =  country.substr(0, pos+2);
+		  country.erase(0, pos+2);
+		  break;
+		}
+
+    	pos = country.find(strgb_guangxi_, 0);
+		if (pos != std::string::npos) {
+		  province =  country.substr(0, pos+4);
+		  country.erase(0, pos+4);
+		  break;
+		}
+
+    	pos = country.find(strgb_neimeng_, 0);
+		if (pos != std::string::npos) {
+		  province =  country.substr(0, pos+6);
+		  country.erase(0, pos+6);
+		  break;
+		}
+
+    	pos = country.find(strgb_ningxia_, 0);
+		if (pos != std::string::npos) {
+		  province =  country.substr(0, pos+4);
+		  country.erase(0, pos+4);
+		  break;
+		}
+
+    	pos = country.find(strgb_xinjiang_, 0);
+		if (pos != std::string::npos) {
+		  province =  country.substr(0, pos+4);
+		  country.erase(0, pos+4);
+		  break;
+		}
+
+    	pos = country.find(strgb_xizang_, 0);
+		if (pos != std::string::npos) {
+		  province =  country.substr(0, pos+4);
+		  country.erase(0, pos+4);
+		  break;
+		}
+    } while(false);
+
+    pos = country.find(strgb_city_, 0);
+    if (pos >= 4 && pos != std::string::npos) {
+      city = country.substr(0, pos+2);
+
+      if (province.empty())
+        province = city;
+    }
+
+    if (!province.empty() && city.empty())
+      city = province;
+    else if (province.empty() && !city.empty())
+      province = city;
+    else if (province.empty() && city.empty())
+      province = city = country;
+    else{}
+
+    std::string out;
+    if (0 == gb2312toutf8(province, out)) {
+      province = out;
+    }
+
+    if (0 == gb2312toutf8(city, out)) {
+      city = out;
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+bool ip_finder::get_ipinfo(const std::string& ipstr, std::string& provincecity)
+{
+  std::string country;
+  std::string area;
+  if (get_ip_original_info(ipstr, country, area)) {
+    size_t pos = country.find(strgb_province_, 0);
+    if (pos >= 4 && pos != std::string::npos) {
+      provincecity =  country.substr(0, pos+2);
+      country.erase(0, pos+2);
+    }
+
+    pos = country.find(strgb_city_, 0);
+    if (pos >= 4 && pos != std::string::npos) {
+      provincecity += country.substr(0, pos+2);
+    }
+
+    if (provincecity.empty())
+      provincecity = country;
+
+    std::string out;
+    if (0 == gb2312toutf8(provincecity, out)) {
+      provincecity = out;
+    } else {
+      provincecity.clear();
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+bool ip_finder::get_ip_original_info(const std::string& ipstr, std::string& country, std::string& area, bool utf8/* = false*/)
+{  
+  if (ipstr.empty() || pshare_ == NULL || pbegin_ == NULL || pend_ == NULL || total_record_ == 0)
+    return false;
+
+  location_t loc;
+	memset(&loc, 0x0, sizeof(location_t)); 
+
+	unsigned char* search = pshare_;
+  unsigned char* pos = pshare_;  
+  unsigned char* firstip = 0;
+ 
+  boost::uint32_t ip = htonl(inet_addr(ipstr.c_str()));  
+  firstip = pbegin_;  
+
+  long l = 0;  
+  long u = total_record_;  
+  long i = 0;  
+  unsigned char* findip = firstip;  
+  boost::uint32_t beginip = 0;  
+  boost::uint32_t endip = 0; 
+
+  //二分法查找  
+  while (l <= u)  
+  {  
+    i = (l + u) / 2;  
+    pos = firstip + i * RECORD_LEN;  
+    beginip = get_long4(pos);  
+    pos += 4;  
+    if (ip < beginip) {  
+      u = i - 1;      
+    } else {  
+      endip = get_long4(search + get_long3(pos));  
+      if (ip > endip) {  
+        l = i + 1;          
+      } else {  
+        findip = firstip + i * RECORD_LEN;  
+        break;      
+      }  
+    }  
+  }
+
+  long offset = get_long3(findip + 4);  
+  pos = search + offset;  
+  endip = get_long4(pos);
+  pos += 4;  
+
+  //boost::uint32_t j = ntohl(beginip);  
+  //inet_ntop(AF_INET, &j, loc.beginip, INET6_ADDRSTRLEN);
+  //j = ntohl(endip);  
+  //inet_ntop(AF_INET, &j, loc.endip, INET6_ADDRSTRLEN);
+
+  unsigned char* byte = pos; // 标志字节
+  pos++;  
+
+  switch (*byte) 
+  {  
+  case REDIRECT_MODE_1:
+    {   
+      long countryOffset = get_long3(pos); // 重定向地址  
+      pos += 3;  
+      pos = search + countryOffset;  
+      byte = pos; // 标志字节
+      pos++;
+
+      switch (*byte) 
+      {  
+      case REDIRECT_MODE_2:
+        {  
+          loc.pcountry_ = search + get_long3(pos);  
+          pos = search + countryOffset + 4;  
+          loc.parea_ = get_area(search, pos);  
+        }  
+        break;  
+      default: 
+        {
+          loc.pcountry_ = byte; 
+					if (loc.pcountry_ && loc.pcountry_[0])
+						loc.parea_ = get_area(search, loc.pcountry_ + strlen((const char*)loc.pcountry_) + 1);  
+					else
+						return false;
+        }  
+        break;  
+      }  
+    }  
+    break;  
+  case REDIRECT_MODE_2:
+    {  
+      loc.pcountry_ = search + get_long3(pos);  
+      loc.parea_ = get_area(search, search + offset + 8); // search + offset + 8;
+    }  
+    break;  
+  default:
+    { 
+      loc.pcountry_ = byte; 
+			if (loc.pcountry_ && loc.pcountry_[0])
+				loc.parea_ = get_area(search, loc.pcountry_ + strlen((const char*)loc.pcountry_) + 1); 
+			else
+				return false;
+    }  
+    break;  
+  }  
+
+	if (loc.pcountry_ && loc.pcountry_[0])
+		country.assign((const char*)loc.pcountry_, strlen((const char*)loc.pcountry_));
+	if (loc.parea_ && loc.parea_[0])
+		area.assign((const char*)loc.parea_, strlen((const char*)loc.parea_));
+
+	if (utf8) {
+		std::string out;
+		if (0 == gb2312toutf8(country, out)) {
+			country = out;
+		} else {
+			country.clear();
+		}
+
+		if (0 == gb2312toutf8(area, out)) {
+			area = out;
+		} else {
+			area.clear();
+		}
+	} 
+
+  return true;
+}  
+
+unsigned char* ip_finder::get_area(unsigned char* pserach, unsigned char* pos) 
+{  
+  unsigned char* byte = pos; // 标志字节
+  pos++; 
+
+  switch (*byte) {
+  //case REDIRECT_MODE_0:
+  //  break;  
+  case REDIRECT_MODE_1:  
+  case REDIRECT_MODE_2: // 标志字节为1或2，表示区域信息被重定向  
+    return pserach + get_long3(pos);  
+    break;  
+  default: // 否则，表示区域信息没有被重定向
+    return byte;  
+    break;  
+  }  
+
+  return NULL;
+}  
+
+boost::uint32_t ip_finder::get_long4(const unsigned char* pos) 
+{  
+  boost::uint32_t result = pos[0];
+	for (boost::uint32_t i = 0; i < 4; i++)
+		result |= ((boost::uint32_t)pos[i])<<(8*i);
+
+  return result; 
+}  
+
+boost::uint32_t ip_finder::get_long3(const unsigned char* pos) 
+{  
+  boost::uint32_t result = pos[0];
+	for (boost::uint32_t i = 0; i < 3; i++)
+		result |= ((boost::uint32_t)pos[i])<<(8*i);
+
+  return result;
+}  
+
+int ip_finder::utf8togb2312(const std::string& instr, std::string& outstr) 
+{ 
+  char outbuf[OUTLEN] = {0};
+  int ret = code_convert("utf-8", "gb2312", instr.c_str(), instr.length(), outbuf, OUTLEN);
+  if (ret == 0) {
+	  outstr = outbuf;
+  }
+  return ret;
+} 
+
+int ip_finder::gb2312toutf8(const std::string& instr, std::string& outstr) 
+{ 
+  char outbuf[OUTLEN] = {0};
+  int ret = code_convert("gb2312", "utf-8", instr.c_str(), instr.length(), outbuf, OUTLEN);
+  if (ret == 0) {
+	outstr = outbuf;
+  }
+
+  return ret;
+} 
+
+int ip_finder::code_convert(const std::string& from_charset, const std::string& to_charset, const char* inbuf, size_t inlen, char* outbuf, size_t outlen) 
+{ 
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+
+#else
+  char** pin = const_cast<char**>(&inbuf); 
+  char** pout = &outbuf; 
+
+  iconv_t cd = iconv_open(to_charset.c_str(), from_charset.c_str()); 
+  
+  if (cd == 0) return -1;
+  
+  memset(outbuf, 0, outlen); 
+  
+  if (iconv(cd, pin, &inlen, pout, &outlen) < 0) 
+    return -1; 
+
+  iconv_close(cd); 
+#endif
+  return 0; 
+} 
+
+std::string wstr2str(const std::wstring& wstr)
+{
+  std::string curLocale = setlocale(LC_ALL, NULL);// curLocale = "C";
+  setlocale(LC_ALL, "chs"); 
+
+  const wchar_t* _Source = wstr.c_str();
+  size_t _Dsize = 2 * wstr.size() + 1;
+  char* _Dest = new char[_Dsize];
+  memset(_Dest, 0, _Dsize);
+  wcstombs(_Dest, _Source, _Dsize);
+  
+  std::string result = _Dest;
+  delete [] _Dest;
+
+  setlocale(LC_ALL, curLocale.c_str());
+  
+  return result;
+}
+
+std::wstring str2wstr(const std::string& str)
+{
+  setlocale(LC_ALL, "chs"); 
+
+  const char* _Source = str.c_str();
+  size_t _Dsize = str.size() + 1;
+  wchar_t *_Dest = new wchar_t[_Dsize];
+
+  wmemset(_Dest, 0, _Dsize);
+  mbstowcs(_Dest, _Source, _Dsize);
+
+  std::wstring result = _Dest;
+  delete [] _Dest;
+
+  setlocale(LC_ALL, "C");
+
+  return result;
+}
+
+}
+}
+}
+
+
---
+++ ip_finder.hpp
@@ -0,0 +1,103 @@
+﻿#ifndef __COMMON_ip_finder_HPP_
+#define __COMMON_ip_finder_HPP_
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+#include <Windows.h>
+#endif
+#include <boost/cstdint.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
+
+namespace is {
+namespace common{
+namespace location{
+#define INET6_ADDRSTRLEN 46  
+
+class ip_finder;
+typedef boost::shared_ptr<ip_finder> ip_finder_ptr;
+
+// only Linux
+class ip_finder : boost::noncopyable
+{
+public:
+  ip_finder();
+  ~ip_finder();
+
+  int initialize(const std::string& file);
+  void destroy();
+
+    
+  // 获得ip所属省、市
+  // 返回数据UTF8编码
+  bool get_ipinfo(const std::string& ipstr, std::string& province, std::string& city);
+
+  // 获得ip所属省市
+  // 返回数据UTF8编码
+  bool get_ipinfo(const std::string& ipstr, std::string& provincecity);
+
+  // 获取IP数据库原始信息
+  // 返回数据GB2312编码
+  bool get_ip_original_info(const std::string& ipstr, std::string& country, std::string& area, bool utf8 = false);
+
+  int utf8togb2312(const std::string& instr, std::string& outstr);
+  int gb2312toutf8(const std::string& instr, std::string& outstr);
+
+  std::string wstr2str(const std::wstring& wstr);
+  std::wstring str2wstr(const std::string& str);
+
+protected:
+  /**  
+   * 返回地区信息  
+   *  
+   * @char *pos 地区的指针  
+   * @return char *  
+   */ 
+  unsigned char* get_area(unsigned char* search, unsigned char* pos);
+
+  //将读取的4个字节转化为长整型数  
+  boost::uint32_t get_long4(const unsigned char* pos);
+
+  //将读取的3个字节转化为长整型数  
+  boost::uint32_t get_long3(const unsigned char* pos);
+
+  int code_convert(const std::string& from_charset, const std::string& to_charset, const char* inbuf, size_t inlen, char* outbuf, size_t outlen);
+
+private:
+  enum {  
+    REDIRECT_MODE_0 = 0x00, // 没有区域信息
+    REDIRECT_MODE_1 = 0x01, // 重定向模式1 偏移量后无地区名
+                          // 标志字节为1，表示国家和区域信息都被同时重定向
+    REDIRECT_MODE_2 = 0x02, // 重定向模式2 偏移量后有地区名  
+                          // 标志字节为2，表示国家信息被重定向
+  }; 
+
+  // 结果集
+  typedef struct   
+  {  
+    unsigned char *pcountry_;
+    unsigned char *parea_;
+    unsigned char beginip[INET6_ADDRSTRLEN]; // 用户IP所在范围的开始地址 
+    unsigned char endip[INET6_ADDRSTRLEN];   // 用户IP所在范围的结束地址
+  } location_t;  
+
+  unsigned char* pshare_;  // 共享内存指针
+  unsigned char* pbegin_;  // 第一条记录指针
+  unsigned char* pend_;		 // 最后一条记录指针
+#if defined(WIN32) || defined(_WIN32) || defined(WINDOWS) || defined(_WINDOWS)
+  HANDLE hFileMap_;
+#endif
+
+  boost::uint32_t total_record_; // 总记录数
+
+  std::string strgb_province_;
+  std::string strgb_city_;
+  std::string strgb_xizang_;
+  std::string strgb_xinjiang_;
+  std::string strgb_neimeng_;
+  std::string strgb_ningxia_;
+  std::string strgb_guangxi_;
+};
+
+}
+}
+}
+#endif
